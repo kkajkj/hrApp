c9fff1f16232483170e9132ccb30bd63f4b122d5
SOHAIL SHAIKH <107352592+kkajkj@users.noreply.github.com>
2024-09-02 13:14:56 +0530

    Update SignupForm.jsx

--- client/src/Components/SignupForm/SignupForm.jsx
+++ client/src/Components/SignupForm/SignupForm.jsx
@@ -44,7 +44,7 @@ const SignupForm = () => {
 
     try {
       // Send form data to the server
-      const response = await fetch("https://hrapp-backend.onrender.com/api/signup", {
+      const response = await fetch("https://hrapp-frontend.onrender.com/api/signup", {
         method: "POST",
         body: data,
          credentials: "include", // If you need to send cookies/auth headers
