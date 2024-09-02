de96fbb3d621fd06e1daaa2f80ad783ee5c54910
SOHAIL SHAIKH <107352592+kkajkj@users.noreply.github.com>
2024-09-02 13:14:05 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin:"https://hrapp-frontend.onrender.com", // your actual frontend URL
+  origin:"https://hrapp-frontend.onrender.com/", // your actual frontend URL
   methods: ["POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
